bbf82af4d7c4f2fa29592f50c0b3d93a2305a042
MicahJank <MicahJank@users.noreply.github.com>
2020-10-22 13:50:47 -0500

    formatted birthday date that appears when a user sets their birthday using the birthday command, also added random color generator for the embeded message color
    Signed-off-by: MicahJank <MicahJank@users.noreply.github.com>

--- src/commands/whatsMyBirthday.js
+++ src/commands/whatsMyBirthday.js
@@ -18,12 +18,17 @@ module.exports = {
         
         let member = dbServer.members.find(m => m.user === message.author.tag)
         let embededMessage;
+
         if(member){
+            // Dates will all come back with the same character count so slicing up to the end of the year seems simplest
+            const formattedDate = member.birthday.toString().slice(0,15);
+            const randomColor = Math.floor(Math.random()*16777215).toString(16);
+            
             embededMessage = Util.embedMessage(
                 'Your Birthday',
                 message.author.tag,
-                '0xffff00',
-                `Your Birthday is set to ${member.birthday}.`                
+                `0x${randomColor}`,
+                `Your Birthday is set to ${formattedDate}.`                
             )
         }else{
             embededMessage = Util.embedMessage(
